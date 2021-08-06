123d6e82a5e94d41a64ec7f34bb058d7b9a9b904
Gareth Davies <hello@garethtdavies.com>
2021-08-06 12:09:43 -0700

    Seperate block rewards and snark work

--- app/forms.py
+++ app/forms.py
@@ -5,4 +5,5 @@ from wtforms.validators import DataRequired, Length
 class TaxForm(FlaskForm):
     address = StringField('address', validators=[DataRequired(), Length(min=55, max=55)])
     export = SelectField('export', choices=[('', 'Choose...'), ('koinly', 'Koinly')], validators=[DataRequired()])
+    export_type = SelectField('export_type', choices=[('', 'Choose...'), ('transactions', 'Transactions')], validators=[DataRequired()])
     submit = SubmitField('Generate Export')
