d57d4042c06e3467ddf1b3951a3dc1e54d1b778c
Gareth Davies <hello@garethtdavies.com>
2021-11-08 10:51:29 -0700

    Revert "Adding support for BigQuery transactions"

--- app/forms.py
+++ app/forms.py
@@ -1,32 +1,27 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, SubmitField, DateField
+from wtforms import StringField, SelectField, SubmitField
 from wtforms.validators import DataRequired, Length
-from wtforms.fields.html5 import DateField
 
 
 class TaxForm(FlaskForm):
     """
     Class to define the front-end tax form
     """
-    address = StringField('Address',
+    address = StringField('address',
                           validators=[DataRequired(),
                                       Length(min=55, max=55)])
-    export = SelectField('Export',
+    export = SelectField('export',
                          choices=[('', 'Choose...'), ('koinly', 'Koinly'),
                                   ('accointing', 'Accointing')],
                          validators=[DataRequired()])
     export_type = SelectField(
-        'Export Type',
+        'export_type',
         choices=
         [('', 'Choose...'),
          ('transactions', 'Transactions (includes staking pool payouts)'),
          ('production',
           'Block Production (only if running a block producer - use coinbase receiver)'
-          ), ('snarks', 'SNARK Work'), ('genesis', 'Genesis Grants')],
+          ), ('snarks', 'SNARK Work'),
+         ('genesis', 'Genesis Grants')],
         validators=[DataRequired()])
-
-    start_date = DateField('Start Date', format="%Y-%m-%d", validators=[DataRequired()])
-
-    end_date = DateField('End Date', format="%Y-%m-%d", validators=[DataRequired()])
-
     submit = SubmitField('Generate Export')
--- app/routes.py
+++ app/routes.py
@@ -3,8 +3,7 @@ from app import app
 from app.forms import TaxForm
 from app.exporters.koinly import Koinly
 from app.exporters.accointing import Accointing
-from datetime import datetime
-import pytz
+
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -15,25 +14,12 @@ def index():
         address = form.address.data
         export = form.export.data
         export_type = form.export_type.data
-        start_date = datetime(form.start_date.data.year,
-                              form.start_date.data.month,
-                              form.start_date.data.day,
-                              tzinfo=pytz.utc)
-        end_date = datetime(form.end_date.data.year,
-                            form.end_date.data.month,
-                            form.end_date.data.day,
-                            tzinfo=pytz.utc)
-
-        start_date = start_date.replace().isoformat()
-        end_date = end_date.replace().isoformat()
 
         if export == "accointing":
             exporter = Accointing()
         else:
             exporter = Koinly()
-        output = make_response(
-            exporter.download_export(address, export_type, start_date,
-                                     end_date))
+        output = make_response(exporter.download_export(address, export_type))
         output.headers[
             "Content-Disposition"] = f"attachment; filename={address}-{export_type}.csv"
         output.headers["Content-type"] = "text/csv"
